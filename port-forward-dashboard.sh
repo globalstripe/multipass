c90c577df4c0e72e345d68e85748b2cb47c221fb
Chris Clark <chris.clark@ostmodern.co.uk>
2024-05-28 12:22:02 +0100

    port forward dashboard

--- port-forward-dashboard.sh
+++ port-forward-dashboard.sh
@@ -1,3 +1,6 @@
 ./token.sh
-echo "Point your local browser at https://192.168.64.10:10443"
+echo "Make sure you have enabled the dashboard: #microks8 enable dashboard"
+echo "Make sure you have enabled ingress: #microk8s enable ingress"
+ip=`multipass info master | grep IPv4 | awk '{ print $2 }'`
+echo "Point your local browser at https://$ip:10443"
 multipass exec master -- sudo /snap/bin/microk8s kubectl port-forward -n kube-system service/kubernetes-dashboard 10443:443 --address 0.0.0.0
--- setup-master.sh
+++ setup-master.sh
@@ -4,6 +4,14 @@ multipass exec master -- sudo iptables -P FORWARD ACCEPT
 echo 'Exec this then exit and shell again'
 multipass exec master -- sudo usermod -aG microk8s ubuntu
 multipass exec master -- sudo chown -f -R ubuntu ~/.kube
-multipass exec master -- echo "alias kubectl='microk8s kubectl'" > ~/.bash_alias
-multipass exec master -- echo "alias k2='microk8s kubectl'" > ~/.bash_alias
+multipass exec master -- bash -c 'microks8 enable dashboard'
+multipass exec master -- bash -c 'microk8s enable ingress'
+echo 'Copy Local File to Multipass Instance'
+multipass transfer .bash_aliases master:.bash_aliases
+multipass exec master -- bash -c 'cat ~/.bash_aliases'
+multipass exec master -- bash -c 'mkdir ingress'
+multipass transfer ingress/ingress-service.yaml master:ingress/ingress-service.yaml
+multipass transfer ingress/apply.cmd  master:ingress/apply.cmd
+multipass exec master -- bash -c 'ls -las  ingress
+#multipass exec master -- bash -c 'microk8s kubectl apply -f ingress/ingress-service.yaml'
 multipass shell master
