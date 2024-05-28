2f217fa7710c7935dc43de9dc186cfec26fdcf0a
Chris Clark <chris.clark@ostmodern.co.uk>
2024-05-28 10:59:38 +0100

    metallb and aliases

---
+++ .bash_aliases
@@ -0,0 +1,4 @@
+alias kubectl='microk8s kubectl'
+alias k='kubectl'
+alias kall='microk8s kubectl get all --all-namespaces'
+alias kpods='microk8s kubectl get po --all-namespaces'
--- setup-master.sh
+++ setup-master.sh
@@ -2,7 +2,8 @@ multipass launch --name master --memory 3G --cpus 2 --disk 50G
 multipass exec master -- sudo snap install microk8s --classic
 multipass exec master -- sudo iptables -P FORWARD ACCEPT
 echo 'Exec this then exit and shell again'
-echo 'sudo usermod -aG microk8s ubuntu'
-read x
-multipass shell master
+multipass exec master -- sudo usermod -aG microk8s ubuntu
+multipass exec master -- sudo chown -f -R ubuntu ~/.kube
+multipass exec master -- echo "alias kubectl='microk8s kubectl'" > ~/.bash_alias
+multipass exec master -- echo "alias k2='microk8s kubectl'" > ~/.bash_alias
 multipass shell master
